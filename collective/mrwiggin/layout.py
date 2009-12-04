9c05619ece5ee7c569ccc6889ae3564c4f20ffc4
Maik Roeder <maikroeder@gmail.com>
2009-12-04 12:21:54 +0000

    fix import

--- collective/mrwiggin/layout.py
+++ collective/mrwiggin/layout.py
@@ -1,7 +1,6 @@
-
+from zope.interface import implements
 from Products.Five import BrowserView
 from collective.mrwiggin.interfaces import ILayout
-
 
 class Layout(BrowserView):
     implements(ILayout)
